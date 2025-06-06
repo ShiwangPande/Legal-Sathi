3084b1d5e4dcbaebd56a0e5c152d506d5478d1f3
ShiwangPande <srpandey@gcoen.ac.in>
2025-06-06 22:34:36 +0530

    added admins only feature

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
       <head>
         <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href="/favicon.svg" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <meta name="apple-mobile-web-app-title" content="Legal Saathi" />
         <link rel="manifest" href="/site.webmanifest" />
